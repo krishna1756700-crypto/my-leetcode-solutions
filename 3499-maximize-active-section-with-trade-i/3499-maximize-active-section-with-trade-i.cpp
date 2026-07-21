6c88ecdeb9fc2f7ba8ea46558544de0e7dcd5fdc
krishna1756700-crypto <krishna1756700@gmail.com>
2026-07-21 19:49:29 +0530

    Time: 60 ms (66.22%), Space: 54.8 MB (76.13%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -2,32 +2,33 @@ class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
         int n=s.size();
-        int ans=0;
         int count=0;
         int prevc=0;
         int maxi=0;
+        int ans=0;
         bool temp=true;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;){
             if(s[i]=='1'){
-              
                 if(count+prevc>maxi)maxi=count+prevc;
                 if(prevc!=0&&count!=0)temp=false;
                 prevc=count;
                 count=0;
-                while(s[i]=='1'){
-                    i++;
+                while(i<n&&s[i]=='1'){
                     ans++;
+                    i++;
                 }
-                i--;
             }
             else{
-                count++;
+                while(i<n&&s[i]=='0'){
+                    count++;
+                    i++;
+                }
             }
         }
         if(count+prevc>maxi)maxi=count+prevc;
         if(prevc!=0&&count!=0)temp=false;
         if(temp)return ans;
-        ans+=maxi;
-        return ans;
+        return ans+maxi;
+
     }
 };
