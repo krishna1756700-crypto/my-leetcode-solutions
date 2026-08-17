21966cfdfd10594c2da3bf9f56aff9d0b019e7e3
krishna1756700-crypto <krishna1756700@gmail.com>
2026-08-17 18:57:51 +0530

    Time: 0 ms (100%), Space: 8.8 MB (41.27%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-  void  get(vector<vector<int>>&a,int k,vector<int>&curr,int n,int sum,int x){
-        if(k==0||x>9||sum>n){
-            if(sum==n&&k==0)a.push_back(curr);
+  void  get(vector<vector<int>>&a,int k,vector<int>&curr,int n,int x){
+        if(k==0||x>9||n<0){
+            if(n==0&&k==0)a.push_back(curr);
             return;
         }
         curr.push_back(x);
-        get(a,k-1,curr,n,sum+x,x+1);
+        get(a,k-1,curr,n-x,x+1);
         curr.pop_back();
-        get(a,k,curr,n,sum,x+1);
+        get(a,k,curr,n,x+1);
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>>a;
         vector<int>curr;
-        get(a,k,curr,n,0,1);
+        get(a,k,curr,n,1);
         return a;
         
     }
