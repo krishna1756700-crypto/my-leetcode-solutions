aa59b895cdd85709ee68222f41f064125bc5c3c2
krishna1756700-crypto <krishna1756700@gmail.com>
2026-08-22 16:08:41 +0530

    Time: 67 ms (30.96%), Space: 151.4 MB (27.42%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,30 +1,39 @@
 class MinStack {
 public:
-    stack<int>s;
-    stack<int>min;
-
+    stack<long long>s;
+    long long minval;
+  
     MinStack() {
-        
+       
     }
     
-    void push(int value) {
-        s.push(value);
-        if(min.empty()||min.top()>=value)min.push(value);
+    void push(long long value) {
+        if(s.empty()){
+            minval=value;
+            s.push(value);
+            
+            return;
+        }
+        if(value>=minval)s.push(value);
+        else{
+            s.push(2*value-minval);
+            minval=value;
+        }
     }
     
     void pop() {
-        if(s.top()==min.top()){
-            min.pop();
-        }
+        if(s.top()<minval)minval=2*minval-s.top();
         s.pop();
     }
     
     int top() {
+        if(s.top()<minval)return minval;
         return s.top();
     }
     
     int getMin() {
-        return min.top();
+        if(s.size()==1)return s.top();
+        return minval;
     }
 };
 
