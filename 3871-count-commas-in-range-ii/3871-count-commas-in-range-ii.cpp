db65e3b2d73d35a5f1c4af15a86b6884b80c849b
krishna1756700-crypto <krishna1756700@gmail.com>
2026-09-09 23:02:40 +0530

    Time: 0 ms (100%), Space: 9.2 MB (19.2%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans=0;
+        if(n-999>0)ans+=n-999;
+        if(n-999999>0)ans+=n-999999;
+        if(n-999999999>0)ans+=n-999999999;
+        if(n-999999999999>0)ans+=n-999999999999;
+        if(n-999999999999999>0)ans+=n-999999999999999;
+        return ans;
+    }
+};
